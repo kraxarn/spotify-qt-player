3b8ef75ff03ca00d98db822ccdc79fdd9c166afa
kraxarn <me@kraxarn.com>
2020-03-12 16:58:38 +0100

    Initial Spotify connection

--- player.js
+++ player.js
@@ -1,8 +1,9 @@
 const os = require("os")
-const exec = require("child_process").exec;
+const exec = require("child_process").exec
 const fs = require("fs")
 const path = require("path")
 
+const status = document.getElementById("status")
 const win = nw.Window.get()
 
 // Create tray icon
@@ -73,4 +74,36 @@ function getAccessToken(callback)
 		// Unsupported
 		callback(null)
 	}
+}
+
+function setStatus(message) {
+	status.textContent += `\n${message}`
+	window.scrollTo(0, document.body.scrollHeight)
+}
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+	setStatus("api ready")
+	const token = location.href.substring(location.href.lastIndexOf("?token=") + 7)
+	setStatus(`using token: ${token}`)
+	const player = new Spotify.Player({
+		name: "spotify-qt",
+		getOAuthToken: cb => { cb(token) }
+	})
+	// Error handling
+	player.addListener('initialization_error', (message) => setStatus(`initialization_error: ${message.message}`))
+	player.addListener('authentication_error', (message) => setStatus(`authentication_error: ${message.message}`))
+	player.addListener('account_error', (message) => setStatus(`account_error: ${message.message}`))
+	player.addListener('playback_error', (message) => setStatus(`playback_error: ${message.message}`))
+	// Playback status updates
+	player.addListener('player_state_changed', (state) => setStatus(`player_state_changed: ${JSON.stringify(state)}`))
+	// Ready
+	player.addListener("ready", (device_id) => {
+		setStatus("ready")
+	})
+	// Not Ready
+	player.addListener('not_ready', ({ device_id }) => {
+		setStatus("offline")
+	})
+	// Connect to the player!
+	player.connect()
 }
